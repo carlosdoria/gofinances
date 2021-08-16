8bcc0acc651a60fe92d77b5f45714ee4da05cabc
carlosdoria <carlosdoria953@gmail.com>
2021-08-16 20:20:49 -0300

    feat: add typescript, styled-components and font poppins

---
+++ src/sreens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+import { Container, Title } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Open up App.js to start working on your app!</Title>
+    </Container>
+  );
+}
