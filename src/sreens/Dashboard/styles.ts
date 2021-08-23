efbd6874ed94115e7d00eb5424f11a8085b6e939
carlosdoria <carlosdoria953@gmail.com>
2021-08-22 22:41:46 -0300

    feat: finish styled components

--- src/sreens/Dashboard/styles.ts
+++ src/sreens/Dashboard/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from '.';
 
 export const Container = styled.View`
     flex: 1;
@@ -12,7 +16,7 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
   background-color: ${({ theme }) => theme.colors.primary};
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
 `;
 
@@ -20,6 +24,7 @@ export const UserWrapper = styled.View`
     width: 100%;
     
     padding: 0 24px;
+    margin-top: ${getStatusBarHeight() + RFValue(28)}px;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
@@ -59,6 +64,31 @@ export const Icon = styled(Feather)`
 
 export const HighlightCards = styled.ScrollView.attrs({
   horizontal: true,
-  showsHorizontalScrollIndicator: true,
+  showsHorizontalScrollIndicator: false,
   contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+  flex: 1%;
+  padding: 0 24px;
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  margin-bottom: 16px;
+`;
+
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+  ).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace()
+  }
 })``;
